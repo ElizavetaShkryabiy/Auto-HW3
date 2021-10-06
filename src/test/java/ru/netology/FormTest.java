c02e7446c68231585201e8f760fd955d8b024099
Elizaveta Shkryabiy <evshkryabiy@gmail.com>
2021-10-06 17:57:46 +0400

    Test added

--- src/test/java/ru/netology/FormTest.java
+++ src/test/java/ru/netology/FormTest.java
@@ -1,5 +1,7 @@
 package ru.netology;
 
+import com.codeborne.selenide.SelenideElement;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,7 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 
 public class FormTest {
@@ -15,7 +20,7 @@ public class FormTest {
     ChromeOptions options = new ChromeOptions();
 
     @BeforeAll
-    static void setUpAll(){
+    static void setUpAll() {
         WebDriverManager.chromedriver().setup();
     }
 
@@ -35,8 +40,13 @@ public class FormTest {
 
     @Test
     public void shouldReturnOK() {
-
-
-
+        driver.get("http://localhost:9999");
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Василий Дорохин");
+        form.$("[data-test-id=phone] input").setValue("+79271112233");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
