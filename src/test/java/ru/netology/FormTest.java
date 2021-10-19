f420cfe8e7eaf12477707112db2e3e914611e5ba
Elizaveta Shkryabiy <evshkryabiy@gmail.com>
2021-10-19 15:27:06 +0400

    Tests for empty fields added, code changed for Selenide set

--- src/test/java/ru/netology/FormTest.java
+++ src/test/java/ru/netology/FormTest.java
@@ -1,49 +1,17 @@
 package ru.netology;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.chrome.ChromeOptions;
-
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class FormTest {
-    WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver","build/tmp/chromedriver.exe");
-        WebDriverManager.chromedriver().setup();
-    }
-
-
-    @BeforeEach
-    void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-    @AfterEach
-    void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
 
     @Test
     public void shouldTestHappyPath() {
-        driver.get("http://localhost:9999");
+
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Василий Пупкин");
@@ -55,44 +23,44 @@ public class FormTest {
 
     @Test
     public void shouldReturnErrorWhenWrongDataIn1stField() {
-        driver.get("http://localhost:9999");
+
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Vasya");
         form.$("[data-test-id=phone] input").setValue("+79271112233");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $(".input_invalid").shouldBe(appear);
+        $(".input_invalid[data-test-id=name]").shouldBe(appear);
     }
 
     @Test
     public void shouldReturnErrorWhenWrongDataIn2ndField() {
-        driver.get("http://localhost:9999");
+
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("+7992112233");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $(".input_invalid").shouldBe(appear);
+        $(".input_invalid[data-test-id=phone]").shouldBe(appear);
     }
 
     @Test
-    public void shouldReturnErrorWhenWrongDataInCheckbox() {
-        driver.get("http://localhost:9999");
+    public void shouldReturnErrorWhenNothingInCheckbox() {
+
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("+79271112233");
         form.$("[data-test-id=agreement]");
         form.$(".button").click();
-        $(".input_invalid").shouldBe(appear);
+        $(".input_invalid[data-test-id=agreement]").shouldBe(appear);
 
     }
 
     @Test
     public void shouldReturnErrorIn1stWhenWrongDataIn1stAnd2ndField() {
-        driver.get("http://localhost:9999");
+
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("Vasya");
@@ -100,6 +68,30 @@ public class FormTest {
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $(".input_invalid[data-test-id=name]").shouldBe(appear);
+    }
+
+    @Test
+    public void shouldReturnErrorWhenEmpty1stField() {
+
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("");
+        form.$("[data-test-id=phone] input").setValue("+79271112233");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(".input_invalid[data-test-id=name]").shouldBe(appear);
+    }
+
+    @Test
+    public void shouldReturnErrorWhenEmpty2ndField() {
+
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(".input_invalid[data-test-id=phone]").shouldBe(appear);
     }
 
 
